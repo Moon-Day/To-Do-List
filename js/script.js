8f90acf87647799828b8a6b2714f7b7175537edb
Guilherme de Souza <97771245+Moon-Day@users.noreply.github.com>
2023-05-09 11:58:33 -0300

    ok

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// selação de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+// funçoes
+
+
+// eventos
+todoForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const inputValue = todoInput.value;
+
+    if(inputValue){
+        console.log(inputValue);
+    }
+});
+
